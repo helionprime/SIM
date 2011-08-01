b1aae1e63f7d1de2f1e435358e88e6eccebbc06d
helion <helion@online.ms>
2011-08-01 16:27:21 +0200

    initial checkin

---
+++ SunGumi/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: sim
+ * Date: 2011-08-01
+ * Time: 4:22 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace SunGumi
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.WriteLine("Simulation finished");
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
